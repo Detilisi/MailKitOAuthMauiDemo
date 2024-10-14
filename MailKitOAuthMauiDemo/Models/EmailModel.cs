df7220427416d4ff3e3691b69a0ed263a296806b
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-10-14 22:18:37 +0200

    Create EmailModel.cs

---
+++ MailKitOAuthMauiDemo/Models/EmailModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MailKitOAuthMauiDemo.Models;
+
+public class EmailModel
+{
+    public string Subject { get; set; }
+    public string From { get; set; }
+    public string Date { get; set; }
+}
