f0e1f98b2c1ad736162d27c4b51870b35175d8ad
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-10-19 23:08:32 +0200

    Update GoogleOAuthService.cs

--- src/MailKitOAuthMauiDemo/Services/GoogleOAuthService.cs
+++ src/MailKitOAuthMauiDemo/Services/GoogleOAuthService.cs
@@ -6,70 +6,69 @@ using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using System.Diagnostics;
 
-namespace MailKitOAuthMauiDemo.Services
+namespace MailKitOAuthMauiDemo.Services;
+
+public class GoogleOAuthService
 {
-    public class GoogleOAuthService
+    public static async Task<UserCredential> GetGoogleUserCredentialAsync
+    (
+        ClientSecrets clientSecrets, 
+        CancellationToken token = default
+    )
     {
-        public static async Task<UserCredential> GetGoogleUserCredentialAsync
-        (
-            ClientSecrets clientSecrets, 
-            CancellationToken token = default
-        )
+        const string anonymousUserId = "anonymous-user";
+        const string cacheFolder = "CredentialCacheFolder";
+
+        try
         {
-            const string anonymousUserId = "anonymous-user";
-            const string cacheFolder = "CredentialCacheFolder";
-
-            try
+            var codeFlow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
             {
-                var codeFlow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
-                {
-                    ClientSecrets = clientSecrets,
-                    DataStore = new FileDataStore(cacheFolder, false),
-                    Scopes = [ PeopleServiceService.Scope.UserinfoEmail, PeopleServiceService.Scope.UserinfoProfile ] 
-                });
+                ClientSecrets = clientSecrets,
+                DataStore = new FileDataStore(cacheFolder, false),
+                Scopes = [ PeopleServiceService.Scope.UserinfoEmail, PeopleServiceService.Scope.UserinfoProfile ] 
+            });
 
-                var codeReceiver = new LocalServerCodeReceiver();
-                var authCode = new AuthorizationCodeInstalledApp(codeFlow, codeReceiver);
-                var userCredential = await authCode.AuthorizeAsync(anonymousUserId, token);
-                
-                return userCredential;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Failed to get Google OAuth2 credentials: {ex.Message}");
-                throw;
-            }
+            var codeReceiver = new LocalServerCodeReceiver();
+            var authCode = new AuthorizationCodeInstalledApp(codeFlow, codeReceiver);
+            var userCredential = await authCode.AuthorizeAsync(anonymousUserId, token);
+            
+            return userCredential;
         }
-
-        public static async Task<EmailAddress> GetGoogleEmailAddressAsync
-        (
-            UserCredential credential, 
-            CancellationToken token = default
-        )
+        catch (Exception ex)
         {
-            const string resourceUrl = "people/me";
-            const string applicationName = "application-name";
-            const string personFields = "emailAddresses, names";
-
-            try
-            {
-                var service = new PeopleServiceService(new BaseClientService.Initializer()
-                {
-                    HttpClientInitializer = credential,
-                    ApplicationName = applicationName,
-                });
-
-                var executer = service.People.Get(resourceUrl);
-                executer.PersonFields = personFields;
-                var profile = await executer.ExecuteAsync(token);
-
-                return profile.EmailAddresses.FirstOrDefault() ?? throw new Exception("Google user email not found");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Failed to get Google user email: {ex.Message}");
-                throw;
-            }   
+            Debug.WriteLine($"Failed to get Google OAuth2 credentials: {ex.Message}");
+            throw;
         }
+    }
+
+    public static async Task<EmailAddress> GetGoogleEmailAddressAsync
+    (
+        UserCredential credential, 
+        CancellationToken token = default
+    )
+    {
+        const string resourceUrl = "people/me";
+        const string applicationName = "application-name";
+        const string personFields = "emailAddresses, names";
+
+        try
+        {
+            var service = new PeopleServiceService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName,
+            });
+
+            var executer = service.People.Get(resourceUrl);
+            executer.PersonFields = personFields;
+            var profile = await executer.ExecuteAsync(token);
+
+            return profile.EmailAddresses.FirstOrDefault() ?? throw new Exception("Google user email not found");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Failed to get Google user email: {ex.Message}");
+            throw;
+        }   
     }
 }
